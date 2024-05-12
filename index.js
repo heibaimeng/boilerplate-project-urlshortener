70db9658f9a2bac1aadd0e6c20a560cecd77e10b
wang huai <heibaimeng@gmail.com>
2024-05-12 11:15:40 +0000

    增加短网址存储和重定向接口

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns')
 const app = express();
-
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -17,6 +19,34 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+const map = {}
+let key = 0
+
+
+app.all('/api/shorturl/:short_url', function(req, res) {
+  const short = req.params.short_url
+  if (map[short]) {
+    res.redirect(map[short])
+  } else {
+    res.json({ error: 'invalid url' })
+  }
+})
+
+app.post('/api/shorturl', function(req, res) {
+  const url = req.body.url
+  const host = url.replace(/^http:\/\//, '')
+  dns.lookup(host, function(err) {
+    if (err) {
+      res.json({ error: 'invalid url' })
+    } else if (map[url]) {
+      res.json({ original_url: url, short_url : map[url]})
+    } else {
+      map[key] = url
+      res.json({ original_url: url, short_url : key++})
+    }
+  })
 });
 
 app.listen(port, function() {
